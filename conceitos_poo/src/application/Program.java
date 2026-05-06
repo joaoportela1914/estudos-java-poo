98d110b58b38687c0b548b2d88fe0e34648a5dd2
João Portela <joao.portela@sempreceub.com>
2026-05-06 20:51:41 -0300

    Encapsulation

--- conceitos_poo/src/application/Program.java
+++ conceitos_poo/src/application/Program.java
@@ -20,7 +20,10 @@ public class Program {
 		double price = sc.nextDouble();
 		Product product = new Product(name, price);
 		
-		
+		product.setName("Computer");
+		System.out.println("Updated name:" + product.getName());
+		product.setPrice(1200.00);
+		System.out.println("Updated price:" + product.getPrice());
 		
 		System.out.println();
 		System.out.println("Product data: " + product);
@@ -28,7 +31,6 @@ public class Program {
 		System.out.println("Enter the number of product to be added in stock:");
 		int quantity = sc.nextInt();
 		product.addProducts(quantity);
-		
 
 		System.out.println();
 		System.out.println("Updated data: " + product);
