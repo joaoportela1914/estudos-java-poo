3ed832c8321fad34c863fd0c4de2786dab19024e
João Portela <joao.portela@sempreceub.com>
2026-05-15 19:57:51 -0300

    vect 1

---
+++ conceitos_poo/src/vetores_application/Vetores_Program.java
@@ -0,0 +1,29 @@
+package vetores_application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Vetores_Program {
+	public static void main(String[] args) {	
+		
+	Locale.setDefault(Locale.US);
+	Scanner sc = new Scanner(System.in);
+		
+	int n = sc.nextInt();
+	double[] vect = new double [n];
+	
+	double sum = 0.0;
+	
+	for( int i=0;i<n; i++) {
+		vect[i] = sc.nextDouble();	
+		sum += vect[i];
+	}
+	  
+	double avg = sum / n;
+	
+	System.out.printf("AVARAGE HEIGHT: %.2f%n" , avg);
+	
+	sc.close();
+	}
+
+}
