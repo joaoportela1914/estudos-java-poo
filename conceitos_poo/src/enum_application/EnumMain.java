68e06b8bc19da0040759074330e4bba77b720f06
João Portela <joao.portela@sempreceub.com>
2026-08-17 16:05:55 -0300

    exercise worker

---
+++ conceitos_poo/src/enum_application/EnumMain.java
@@ -0,0 +1,24 @@
+package enum_application;
+
+import java.util.Date;
+
+import enum_entities.Order;
+import enum_entities.OrderStatus;
+
+public class EnumMain {
+
+	public static void main(String[] args) {
+
+		Order order = new Order(1080, new Date(), OrderStatus.PENDING_PAYMENT);
+		
+		System.out.println(order);
+		
+		OrderStatus os1 = OrderStatus.DELIVERED;
+		
+		OrderStatus os2 = OrderStatus.valueOf("DELIVERED");
+		
+		System.out.println(os1);
+		System.out.println(os2);
+	}
+
+}
