861bf8730957c0132fed48259ad5bb5cbbd9a66d
João Portela <joao.portela@sempreceub.com>
2026-08-31 16:28:03 -0300

    polyformism exercise 1

--- conceitos_poo/src/inheritance_entities/SavingsAccount.java
+++ conceitos_poo/src/inheritance_entities/SavingsAccount.java
@@ -1,6 +1,6 @@
 package inheritance_entities;
 
-public class SavingsAccount extends Account{
+public final class SavingsAccount extends Account{
 	
 	private Double interestRate;
 	
@@ -25,7 +25,7 @@ public class SavingsAccount extends Account{
 		balance += balance * interestRate;
 	}
 	@Override
-	public void withdraw(double amount) {
+	public final void withdraw(double amount) {
 		balance -= amount;
 	}
 }
